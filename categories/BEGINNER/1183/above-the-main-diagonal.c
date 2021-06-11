0e2f62c4d0f8f6852a12729a3d680aed79a74d25
Jonatha Costa Lima <jonathaclima@icloud.com>
2021-06-11 09:07:48 -0300

    Above the Main Diagonal

---
+++ categories/BEGINNER/1183/above-the-main-diagonal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    float m[12][12], value, sum = 0;
+    char operation;
+
+    scanf("%c", &operation);
+
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            scanf("%f", &value);
+            m[i][j] = value;
+            if (i < j) {
+                sum += m[i][j];
+            }
+        }
+    }
+
+    operation == 'S' ? printf("%.1f\n", sum) : printf("%.1f\n", sum / 66);
+
+    return 0;
+}
