78ab0ebd1d70f5a21662fdad5230e7d0237e0448
Jonatha Costa Lima <jonathaclima@icloud.com>
2021-06-30 16:05:26 -0300

    Square Matrix I

---
+++ categories/BEGINNER/1435/square-matrix-I.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main () {
+    int n, i, j;
+    
+    while (n > 0) {
+        if (scanf("%d", &n) == 0) break;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n; j++) {
+                if (i <= j) {
+                    if (i + j <= n)
+                        printf("%3d ", i);
+                    else
+                        j == n ? printf("  1\n") : printf("%3d ", (n - j) + 1);
+                }
+                else 
+                    i + j <= n ? printf("%3d ", j) : printf("%3d ", (n - i) + 1);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
