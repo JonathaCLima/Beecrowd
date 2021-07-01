acb60fd8475a88bbb76b3fdf2eafc94643d12386
Jonatha Costa Lima <jonathaclima@icloud.com>
2021-07-01 16:02:43 -0300

    Square Matrix II

---
+++ categories/BEGINNER/1478/square-matrix-II.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+    int n, i, j;
+
+    while (n > 0) {
+        if (scanf("%d", &n) == 0) break;
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++)
+                j < n - 1 ? printf("%3d ", 1 + abs(i - j)) : printf("%3d\n", 1 + abs(i - j));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
