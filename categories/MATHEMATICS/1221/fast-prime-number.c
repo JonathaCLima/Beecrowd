b280bb131e58fd11693adf3a713d317e457f17cb
Jonatha Costa Lima <jonathaclima@icloud.com>
2022-02-21 12:51:39 -0300

    Fast Prime Number

--- categories/MATHEMATICS/1221/fast-prime-number.c
+++ categories/MATHEMATICS/1221/fast-prime-number.c
@@ -9,18 +9,21 @@ int main() {
     scanf("%d", &n);
 
     while(n--) {
-        int x, prime = True;
+        int x;
 
         scanf("%d", &x);
 
         if((x != 2 && x % 2 == 0) || x == 1)
-            prime = False;
+            printf("Not Prime\n");
+        else {
+            boolean prime = True;
 
-        for(int i = 3; i <= sqrt(x); i += 2)
-            if(x % i == 0)
-                prime = False;
+            for(int i = 3; prime == True && i <= sqrt(x); i += 2)
+                if(x % i == 0)
+                    prime = False;
 
-        prime == True ? printf ("Prime\n") : printf("Not Prime\n");
+            prime == True ? printf("Prime\n") : printf("Not Prime\n");
+        }
     }
 
     return 0;
