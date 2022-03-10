fc4db1d0d1c1c7dd40ea64c5ec8240e8cbdc12e4
Jonatha Costa Lima <jonathaclima@icloud.com>
2022-03-10 13:01:14 -0300

    Zero or One

--- categories/AD-HOC/1467/zero-or-one.cpp
+++ categories/AD-HOC/1467/zero-or-one.cpp
@@ -2,14 +2,20 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     int a, b, c;
 
-    while(cin >> a >> b >> c) {
+    while(cin >> a >> b >> c)
+    {
         if(a != b)
+        {
             cout << (a != c ? "A" : "B") << endl;
+        }
         else
+        {
             cout << (a != c ? "C" : "*") << endl;
+        }
     }
 
     return 0;
