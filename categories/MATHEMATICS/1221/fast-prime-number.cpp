03d59b75b128985cf4c009450dea9da807c70cb0
Jonatha Costa Lima <jonathaclima@icloud.com>
2022-03-11 22:04:14 -0300

    Fast Prime Number

--- categories/MATHEMATICS/1221/fast-prime-number.cpp
+++ categories/MATHEMATICS/1221/fast-prime-number.cpp
@@ -3,28 +3,34 @@
 
 using namespace std;
 
-typedef enum {False, True} boolean;
-
-int main() {
+int main()
+{
     int n;
 
     cin >> n;
 
-    while(n--) {
+    while(n--)
+    {
         int x;
 
         cin >> x;
 
         if((x != 2 && x % 2 == 0) || x == 1)
+        {
             cout << "Not Prime" << endl;
-        else {
-            boolean prime {True};
+        }
+        else
+        {
+            bool prime = true;
 
-            for(int i = 3; prime == True && i <= sqrt(x); i += 2)
+            for(int i = 3; prime == true && i <= sqrt(x); i += 2)
+            {
                 if(x % i == 0)
-                    prime = False;
-
-            cout << ((prime == True) ? "Prime" : "Not Prime") << endl;
+                {
+                    prime = false;
+                }
+            }
+            cout << ((prime == true) ? "Prime" : "Not Prime") << endl;
         }
     }
 
