2cd83888befd69e3ecd13a49b0cc2ade53470987
Jonatha Costa Lima <jonathaclima@icloud.com>
2022-03-15 17:02:48 -0300

    Extremely Basic

---
+++ categories/BEGINNER/1001/extremely-basic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a {0}, b {0}, x {0};
+
+    cin >> a >> b;
+    x = a + b;
+    cout << "X = " << x << endl;
+
+    return 0;
+}
