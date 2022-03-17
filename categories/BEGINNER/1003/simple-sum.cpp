b94d24e1b6e0b632a605ff06c4cbd1ba6517b931
Jonatha Costa Lima <jonathaclima@icloud.com>
2022-03-17 13:38:30 -0300

    Simple Sum

---
+++ categories/BEGINNER/1003/simple-sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a {0}, b {0}, sum {0};
+
+    cin >> a >> b;
+    sum = a + b;
+    cout << "SOMA = " << sum << endl;
+
+    return 0;
+}
