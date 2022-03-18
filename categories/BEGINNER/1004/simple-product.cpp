77b06cfe26cae7de3bc04946b689499f388a6d00
Jonatha Costa Lima <jonathaclima@icloud.com>
2022-03-18 13:15:08 -0300

    Simple Product

---
+++ categories/BEGINNER/1004/simple-product.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a {0}, b {0}, prod {0};
+
+    cin >> a >> b;
+    prod = a * b;
+    cout << "PROD = " << prod << endl;
+
+    return 0;
+}
