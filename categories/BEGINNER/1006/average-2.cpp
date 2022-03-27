9863fa03612a958624cc1f037d30df8f87b8790e
Jonatha Costa Lima <jonathaclima@icloud.com>
2022-03-27 14:04:13 -0300

    Average 2

--- categories/BEGINNER/1006/average-2.cpp
+++ categories/BEGINNER/1006/average-2.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main()
 {
-    double a {0}, b {0}, c {0}, average {0};
+    double a, b, c;
 
     cin >> a >> b >> c;
-    average = ((a * 2) + (b * 3) + (c * 5)) / (2 + 3 + 5);
+    int average {((a * 2) + (b * 3) + (c * 5)) / (2 + 3 + 5)};
     cout << fixed << setprecision(1) << "MEDIA = " << average << endl;
 
     return 0;
