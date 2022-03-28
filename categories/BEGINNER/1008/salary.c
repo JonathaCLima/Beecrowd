aaa636f4f3a5357ed54211f7ddf528a39588ddea
Jonatha Costa Lima <jonathaclima@icloud.com>
2022-03-28 13:12:21 -0300

    Salary

--- categories/BEGINNER/1008/salary.c
+++ categories/BEGINNER/1008/salary.c
@@ -4,9 +4,9 @@ int main() {
     double worked_hours, salary, hour_value;
     int employee_number;
 
-    scanf("%ld %lf %lf", &employee_number, &worked_hours, &hour_value);
+    scanf("%d %lf %lf", &employee_number, &worked_hours, &hour_value);
     salary = worked_hours * hour_value;
-    printf("NUMBER = %.0ld\nSALARY = U$ %.2lf\n", employee_number, salary);
+    printf("NUMBER = %d\nSALARY = U$ %.2lf\n", employee_number, salary);
 
     return 0;
 }
