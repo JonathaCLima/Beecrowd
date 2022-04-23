e5139aa36e3d6df9518bdd28ef4e365c8049c86e
Jonatha Costa Lima <jonathaclima@icloud.com>
2022-04-22 22:56:51 -0300

    Game Time with Minutes

--- categories/BEGINNER/1047/game-time-with-minutes.c
+++ categories/BEGINNER/1047/game-time-with-minutes.c
@@ -1,41 +1,36 @@
 #include <stdio.h>
 
 int main() {
-    int initial_hour, initial_minute, final_hour, final_minute, hour, minute;
+    int initial_hour, initial_minute, final_hour, final_minute, hour = 0, minute = 0;
 
     scanf("%d %d %d %d", &initial_hour, &initial_minute, &final_hour, &final_minute);
-
-    initial_minute = (24 * 60) - ((initial_hour * 60) + initial_minute);
-    final_minute   = (24 * 60) - ((final_hour   * 60) + final_minute);
-
-    if ((initial_minute - final_minute) == 0) {
+    
+    int total_initial_minute = (24 * 60) - ((initial_hour * 60) + initial_minute);
+    int total_final_minute   = (24 * 60) - ((final_hour   * 60) + final_minute);
+    
+    if ((total_initial_minute - total_final_minute) == 0) {
         hour = 24;
         minute = 0;
     }
-    else {
-        if ((initial_minute - final_minute) > 0) {
-            while ((initial_minute - final_minute) >= 60) {
-                hour += 1;
-                initial_minute -= 60;
-            }
-            minute = (initial_minute - final_minute);
+    else if ((total_initial_minute - total_final_minute) > 0) {
+        while ((total_initial_minute - total_final_minute) >= 60) {
+            hour += 1;
+            total_initial_minute -= 60;
         }
-        else {
-            if ((initial_minute - final_minute) < 0) {
-                initial_minute = ((-1) * (initial_minute));
-                final_minute   = ((-1) * (final_minute));
-                while ((initial_minute - final_minute) > 60) {
-                    hour += 1;
-                    initial_minute -= 60;
-                }
-                minute = (initial_minute - final_minute);
-                hour   = (hour   * (-1) + 24);
-                minute = (minute * (-1));
-                while ((minute < 0) && (hour > 0)) {
-                    hour -= 1;
-                    minute += 60;
-                }
-            }
+        minute = (total_initial_minute - total_final_minute);
+    }
+    else {
+        total_initial_minute *= -1;
+        total_final_minute   *= -1;
+        while ((total_initial_minute - total_final_minute) > 60) {
+            hour += 1;
+            total_initial_minute -= 60;
+        }
+        minute = (total_initial_minute - total_final_minute) * (-1);
+        hour = (hour * (-1)) + 24;
+        while ((minute < 0) && (hour > 0)) {
+            hour -= 1;
+            minute += 60;
         }
     }
 
