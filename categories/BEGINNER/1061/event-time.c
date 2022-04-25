daccfe32f01f67be961342a963ccfc2b2625fcb4
Jonatha Costa Lima <jonathaclima@icloud.com>
2022-04-25 14:00:40 -0300

    Event Time

--- categories/BEGINNER/1061/event-time.c
+++ categories/BEGINNER/1061/event-time.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 
 int main() {
-    int start_day, start_hour, start_minute, start_second, end_day, end_hour, end_minute, end_second, duration_day, duration_hour, duration_minute, duration_second;
+    int start_day, start_hour, start_minute, start_second;
+    int end_day, end_hour, end_minute, end_second;
 
-    scanf("Dia %d%*c%d : %d : %d%*cDia %d%*c%d : %d : %d", &start_day, &start_hour, &start_minute, &start_second, &end_day, &end_hour, &end_minute, &end_second);
+    scanf("Dia %d", &start_day);
+    scanf("%d : %d : %d\n", &start_hour, &start_minute, &start_second);
+    scanf("Dia %d", &end_day);
+    scanf("%d : %d : %d", &end_hour, &end_minute, &end_second);
 
-    duration_day = end_day - start_day;
+    int duration_day = end_day - start_day;
+    int duration_hour = end_hour - start_hour;
+    int duration_minute = end_minute - start_minute;
+    int duration_second = end_second - start_second;
 
-    if (start_hour <= end_hour)
-        duration_hour = end_hour - start_hour;
-    else if (start_hour > end_hour) {
-        duration_hour = (24 - (start_hour - end_hour));
-        duration_day -= 1;
+    if (duration_second < 0) {
+        duration_second += 60;
+        duration_minute--;
     }
 
-    if (start_minute <= end_minute)
-        duration_minute = end_minute - start_minute;
-    else if (start_minute > end_minute) {
-        duration_minute = (60 - (start_minute - end_minute));
-        duration_hour -= 1;
+    if (duration_minute < 0) {
+        duration_minute += 60;
+        duration_hour--;
     }
 
-    if (start_second <= end_second)
-        duration_second = end_second - start_second;
-    else if (start_second > end_second) {
-        duration_second = (60 - (start_second - end_second));
-        duration_minute -= 1;
+    if (duration_hour < 0) {
+        duration_hour += 24;
+        duration_day--;
     }
 
-    printf("%d dia(s)\n%d hora(s)\n%d minuto(s)\n%d segundo(s)\n", duration_day, duration_hour, duration_minute, duration_second);
+    printf("%d dia(s)\n", duration_day);
+    printf("%d hora(s)\n", duration_hour);
+    printf("%d minuto(s)\n", duration_minute);
+    printf("%d segundo(s)\n", duration_second);
 
     return 0;
 }
