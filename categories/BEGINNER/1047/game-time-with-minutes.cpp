76e7fe61a7528ab3d17392720d092a599965cd89
Jonatha Costa Lima <jonathaclima@icloud.com>
2022-04-27 16:45:24 -0300

    Game Time with Minutes

---
+++ categories/BEGINNER/1047/game-time-with-minutes.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int initial_hour, initial_minute, final_hour, final_minute, hour {0}, minute {0};
+
+    cin >> initial_hour >> initial_minute >> final_hour >> final_minute;
+
+    int total_initial_minute {(24 * 60) - ((initial_hour * 60) + initial_minute)};
+    int total_final_minute   {(24 * 60) - ((final_hour   * 60) + final_minute)};
+    
+    if ((total_initial_minute - total_final_minute) == 0)
+    {
+        hour = 24;
+        minute = 0;
+    }
+    else if ((total_initial_minute - total_final_minute) > 0)
+    {
+        while ((total_initial_minute - total_final_minute) >= 60)
+        {
+            hour += 1;
+            total_initial_minute -= 60;
+        }
+        minute = (total_initial_minute - total_final_minute);
+    }
+    else
+    {
+        total_initial_minute *= -1;
+        total_final_minute   *= -1;
+        while ((total_initial_minute - total_final_minute) > 60)
+        {
+            hour += 1;
+            total_initial_minute -= 60;
+        }
+        minute = (total_initial_minute - total_final_minute) * (-1);
+        hour = (hour * (-1)) + 24;
+        while ((minute < 0) && (hour > 0))
+        {
+            hour -= 1;
+            minute += 60;
+        }
+    }
+
+    cout << "O JOGO DUROU " << hour << " HORA(S) E " << minute << " MINUTO(S)" << endl;
+
+    return 0;
+}
