4e5d585795811b6d517a13b19450f4df94694d63
Jonatha Costa Lima <jonathaclima@icloud.com>
2022-07-15 07:16:28 -0300

    Above the Secundary Diagonal

---
+++ categories/BEGINNER/1185/above-the-secundary-diagonal.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    float m[12][12], sum {0};
+    char operation;
+
+    cin >> operation;
+
+    for (int i {0}; i < 12; i++)
+    {
+        for (int j {0}; j < 12; j++)
+        {
+            float value;
+
+            cin >> value;
+
+            m[i][j] = value;
+
+            if (i < 11 - j)
+            {
+                sum += m[i][j];
+            }
+        }
+    }
+
+    if (operation == 'M')
+    {
+        sum /= 66;
+    }
+
+    cout << fixed << setprecision(1) << sum << endl;
+
+    return 0;
+}
