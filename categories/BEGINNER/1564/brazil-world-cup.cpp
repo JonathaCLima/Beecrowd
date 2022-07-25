98cd4523cc8abd8ddc739dc417ff7b8a8187ed52
Jonatha Costa Lima <jonathaclima@icloud.com>
2022-07-25 11:03:30 -0300

    Brazil World Cup

---
+++ categories/BEGINNER/1564/brazil-world-cup.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    while (cin >> n)
+    {
+        if (n == 0)
+        {
+            cout << "vai ter copa!" << endl;
+        }
+        else
+        {
+            cout << "vai ter duas!" << endl;
+        }
+    }
+
+    return 0;
+}
