207e5380e213478d91193e14be137b8a6608a65d
Jonatha Costa Lima <jonathaclima@icloud.com>
2022-07-27 10:27:01 -0300

    Array 123

---
+++ categories/BEGINNER/1534/array-123.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    while (scanf("%d", &n) != EOF)
+    {
+        for (int i {0}; i < n; i++)
+        {
+            for (int j {0}; j < n; j++)
+            {
+                if ((i == j) && (n % 2 == 0) || (i == j) && (j != n / 2))
+                {
+                    cout << 1;
+                }
+                else if (i + j == n - 1)
+                {
+                    cout << 2;
+                }
+                else
+                {
+                    cout << 3;
+                }
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
