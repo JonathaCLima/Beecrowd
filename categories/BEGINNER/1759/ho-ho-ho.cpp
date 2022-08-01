8f1fd5dc9ede2718aaec6dbbf86420a56b745fe0
Jonatha Costa Lima <jonathaclima@icloud.com>
2022-08-01 16:38:22 -0300

    Ho Ho Ho

---
+++ categories/BEGINNER/1759/ho-ho-ho.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int quantity;
+
+    cin >> quantity;
+
+    for (int i {1}; i < quantity; i++)
+    {
+        cout << "Ho ";
+    }
+
+    cout << "Ho!" << endl;
+
+    return 0;
+}
